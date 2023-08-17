76807e6c4de7bbf2a356bd1cfba6cbce1a12f3de
prayudo27 <prayudo27@gmail.com>
2023-08-17 11:42:27 +0700

    set message form contact form to email with emailJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,90 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu')
-const navToggle = document.getElementById('nav-toggle')
-const navClose = document.getElementById('nav-close')
-      
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
 /*=============== MENU SHOW ===============*/
 /* Validate of constant exist */
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /*=============== MENU HIDDEN ===============*/
 /* Validate of contant exist */
-if(navClose){
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove teh show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove teh show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const blurHeader = () => {
-    const header = document.getElementById('header')
-    //When the scroll os greater than 50 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('blur-header')
-                       : header.classList.remove('blur-header')
-}
-window.addEventListener('scroll', blurHeader)
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  //When the scroll os greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== ADD BLUR TO HEADER ===============*/
-
-
+const blurHeader = () => {
+  const header = document.getElementById("header");
+  //When the scroll os greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form");
+const contactMessage = document.getElementById("contact-message");
 
+const sendEmail = (e) => {
+  e.preventDefault();
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm(
+      "service_0nmdv7w",
+      "template_6xdaset",
+      "#contact-form",
+      "fGxJsWF1yznqZ4piB"
+    )
+    .then(
+      () => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
 
-/*=============== SHOW SCROLL UP ===============*/ 
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
 
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        //Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
+
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
