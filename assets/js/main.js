e005a21bc70c36bed0fdc7332f4e94d1a0a793b6
prayudo27 <prayudo27@gmail.com>
2023-08-18 15:58:33 +0700

    add script for animation scroll and animation webview

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,49 @@ const sendEmail = (e) => {
 contactForm.addEventListener("submit", sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  //When teh scroll is higher tah 350 viewport height, add teh show-scroll class to the tah with the scrollup
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  //   reset: true //Animation repeat
+});
+
+sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`);
+sr.reveal(`.home__image`, { origin: "bottom" });
+sr.reveal(`.about__data, .skills__data`, { origin: "left" });
+sr.reveal(`.image__image, .skills__content`, { origin: "right" });
+sr.reveal(`.services__card, .projects__card`, { interval: 100 });
